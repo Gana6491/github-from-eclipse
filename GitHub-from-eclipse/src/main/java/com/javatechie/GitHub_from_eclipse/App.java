2688070e45cf23875fe39a64ee3cf8e14b4a16d9
Raju <Raju@192.168.0.104>
2022-03-18 18:54:22 +0530

    Test commit to git hub

--- GitHub-from-eclipse/src/main/java/com/javatechie/GitHub_from_eclipse/App.java
+++ GitHub-from-eclipse/src/main/java/com/javatechie/GitHub_from_eclipse/App.java
@@ -6,8 +6,16 @@ package com.javatechie.GitHub_from_eclipse;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String args[]) {
+    	int n1=0,n2=1,n3;
+    	
+    	System.out.println(n1 +" " + n2);
+    	
+    	for(int i= 2;i<10;i++) {
+    		n3 = n1+n2;
+    		System.out.print(" "+n3);
+    		n1=n2;
+    		n2=n3;
+    	}         
     }
 }
